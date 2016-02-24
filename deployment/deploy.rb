6792eccb425bcb20beb56815fc0a79242aa6bfc0
Nguyen Nhu Tuan <tuanquynh0508@gmail.com>
2016-02-24 16:39:13 +0700

    Fix deploy script

--- deployment/deploy.rb
+++ deployment/deploy.rb
@@ -1,42 +1,33 @@
+#Deployment for Symfony version 3
 lock '3.4.0'
 
-set :application,			'sym3cms'
-set :stages,        		["staging", "production"]
-set :default_stage, 		"staging"
-set :repo_url, 				'https://github.com/tuanquynh0508/sym3lab.git'
-set :branch,        		ENV['BRANCH'] || 'master'
-set :deploy_to,     		"/data/www/#{fetch(:application)}"
-set :scm,           		:git
-set :use_sudo,      		false
-set :format, 				:pretty
-set :log_level, 			:debug
-set :keep_releases, 		5
+set :application,				'sym3cms'
+set :stages,					["staging", "production"]
+set :default_stage,				"staging"
 
-##############################################
+set :repo_url,					'https://github.com/tuanquynh0508/sym3lab.git'
+set :branch,					ENV['BRANCH'] || 'master'
+set :scm,						:git
+
+set :deploy_to,					"/data/www/#{fetch(:application)}"
+set :use_sudo,					false
+set :format,					:pretty
+set :log_level,					:debug
+set :keep_releases,				5
+
 #SUPPORT FOR SYMFONY 3
-set :symfony_console_path, "bin/console"
-set :log_path,              "var/logs"
-set :cache_path,            "var/cache"
-set :sessions_path,		   "var/sessions"
-#set :app_config_file,       fetch(:app_path) + "/config/parameters.yml"
-##############################################
+set :symfony_console_path,		"bin/console"
+set :log_path,					"var/logs"
+set :cache_path,				"var/cache"
+set :sessions_path,				"var/sessions"
 
-# Controllers to clear
-#set :controllers_to_clear, ["app_*.php"]
-
-#set :linked_files, 			%w{app/config/parameters.yml}
-set :file_permissions_paths,    [fetch(:log_path), fetch(:cache_path), fetch(:sessions_path)]
-set :writable_dirs, 		[fetch(:log_path), fetch(:cache_path), fetch(:sessions_path)]
-#fetch(:web_path) + "/uploads"
-set :linked_dirs,           [fetch(:log_path), "vendor"]
-#set :linked_files,          [fetch(:release_path) + "app/config/parameters.yml"]
-set :file_permissions_users,['apache']
-set :webserver_user,        "apache"
-set :permission_method,     :acl
-set :use_set_permissions,   true
-
-set :composer_install_flags, '--no-dev --no-interaction --optimize-autoloader'
-SSHKit.config.command_map[:composer] = "php -d memory_limit=1G #{shared_path.join("composer.phar")}"
+set :file_permissions_paths,	[fetch(:log_path), fetch(:cache_path), fetch(:sessions_path)]
+set :writable_dirs,				[fetch(:log_path), fetch(:cache_path), fetch(:sessions_path)]
+set :linked_dirs,				[fetch(:log_path), "vendor"]
+set :file_permissions_users,	['apache']
+set :webserver_user,			"apache"
+set :permission_method,			:acl
+set :use_set_permissions,		true
 
 namespace :deploy do
   after :starting, 'composer:install_executable'
--- deployment/stages/staging.rb
+++ deployment/stages/staging.rb
@@ -1,11 +1,12 @@
 set :stage, :staging
 set :symfony_env, "dev"
 
+# edit IP / Port and SSH user of your staging server
+server '10.190.5.65', user: 'root', port: 22, roles: %w{app db web}
+
 set :branch, 'master' # your development branch
 #set :deploy_to, '/data/www/sym3cms' # path on staging server
 
-#set :controllers_to_clear, []
-#set :composer_install_flags, '--prefer-dist --no-interaction --optimize-autoloader'
-
-server '10.190.5.65', user: 'root', port: 22, roles: %w{app db web}# edit IP / Port and SSH user of your staging server
-#SSHKit.config.command_map[:composer] = "php #{shared_path.join("composer.phar")}"
+set :controllers_to_clear, []
+set :composer_install_flags, '--prefer-dist --no-interaction --optimize-autoloader'
+SSHKit.config.command_map[:composer] = "php -d memory_limit=1G #{shared_path.join("composer.phar")}"
