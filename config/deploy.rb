5cd63ddffd892a9f15e7be7b621170a87847df0e
Nguyen Nhu Tuan <tuanquynh0508@gmail.com>
2016-02-24 13:18:28 +0700

    change deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,16 +1,45 @@
 lock '3.4.0'
 
-set :application, 'sym3cms'
-set :repo_url, 'https://github.com/tuanquynh0508/sym3lab.git'
-set :linked_files, %w{app/config/parameters.yml}
-set :linked_dirs, %w{var/logs vendor web/vendor node_modules web/assets}
+set :application,			'sym3cms'
+set :stages,        		["dev", "rec", "prod"]
+set :default_stage, 		"dev"
+set :repo_url, 				'https://github.com/tuanquynh0508/sym3lab.git'
+set :branch,        		ENV['BRANCH'] || 'master'
+set :deploy_to,     		"/data/www/#{fetch(:application)}"
+set :scm,           		:gitcopy
+set :use_sudo,      		false
+set :keep_releases, 		4
 
-set :format, :pretty
-set :log_level, :debug
-set :keep_releases, 5
+set :linked_dirs,   		["var", "vendor"]
+set :file_permissions_users,['apache']
+set :webserver_user,        "apache"
+set :permission_method,     :acl
+set :use_set_permissions,   true
 
-after 'deploy:starting', 'composer:install_executable'
-#after 'deploy:updated', 'npm:install'   # remove it if you don't use npm
-#after 'deploy:updated', 'bower:install' # remove it if you don't use bower
-after 'deploy:updated', 'symfony:assets:install'
-after 'deploy:updated', 'symfony:assetic:dump'
+set :composer_install_flags, '--no-dev --no-interaction --optimize-autoloader'
+
+SSHKit.config.command_map[:composer] = "php -d memory_limit=1G #{shared_path.join("composer.phar")}"
+
+namespace :deploy do
+  after :starting, 'composer:install_executable'
+end
+
+namespace :deploy do
+  #task :migrate do
+  #  invoke 'symfony:console', 'doctrine:migrations:migrate', '--no-interaction'
+  #end
+
+  desc "Set the correct permissions for resource data folder"
+  task :fix_permissions do
+    on roles(:app) do
+      execute "chmod -R 777 #{release_path}/var/Resources/data/"
+      execute "chmod -R 777 #{release_path}/var/Resources/data/*"
+      execute "chmod -R 777 #{release_path}/var/Resources/test.db"
+    end
+  end
+end
+
+after 'deploy:updated',   'symfony:assets:install'
+after 'deploy:updated',   'symfony:assetic:dump'
+#after 'deploy:updated',   'deploy:migrate'
+after 'deploy:finished',  'deploy:fix_permissions'
